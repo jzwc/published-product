53013d68a2babc473d0d31cd5c76dfd75bbb6a84
Pavel Kroh <pkroh@cisco.com>
2023-10-07 17:13:57 +0200

    Adding Dummy wrapper

--- Package.swift
+++ Package.swift
@@ -9,14 +9,23 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "PublishedProductPackage",
-            targets: ["PublishedFramework"]
+            targets: ["PublishedFramework", "DummyWrapper"]
         )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/jzwc/external-package.git", branch: "main")
     ],
     targets: [
         .binaryTarget(
             name: "PublishedFramework",
             url: "https://sdk.smartlook.com/ios/others/PublishedFramework4.zip",
             checksum: "bd8866f9fc0d569d0e856eef345af7462a9bea14728171ae4286fcad000201ed"
+        ),
+        .target(
+            name: "DummyWrapper",
+            dependencies: [
+                .product(name: "ExternalPackage", package: "external-package")
+            ]
         ),
     ]
 )
---
+++ Sources/DummyWrapper/DummyWrapper.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by pkroh on 07.10.2023.
+//
+
+import Foundation
+
+import ExternalPackage
+
+struct DummyWrapper {}
